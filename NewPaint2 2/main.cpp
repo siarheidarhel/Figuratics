c93998d956ff570a3d8525ed1c993d992a446f76
Siarhei Darhel <83512457+siarheidarhel@users.noreply.github.com>
2021-07-14 22:07:10 +0300

    change colour a bit

--- NewPaint2 2/main.cpp
+++ NewPaint2 2/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QPalette myPallete (Qt::red,Qt::lightGray);
+    a.setPalette(myPallete);
     w.show();
 
     return a.exec();
